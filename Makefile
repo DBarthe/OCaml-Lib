e47fa439b55e94dce5783c57b9ccd57a00bcdf79
Barthelemy Delemotte <barthelemy.delemotte@gmail.com>
2014-11-13 21:53:38 +0100

    add unit tests

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 OCAMLMAKEFILE = OCamlMakefile
 
 SOURCES_DIR = src
-MODULES = option binomialHeap priorityQueue
+MODULES = option utils binomialHeap priorityQueue
 SOURCES = $(foreach m,$(MODULES),$(SOURCES_DIR)/$(m).ml)
 
 RESULT = libmy
@@ -9,6 +9,14 @@ RESULT = libmy
 DOC_FILES = $(wildcard $(SOURCES_DIR)/*.mli)
 OCAMLDOCFLAGS = -charset utf8
 
+TEST_SRC = $(SOURCES_DIR)/test.ml
+
 all: native-code-library byte-code-library
+
+test: byte-code-library
+	ocamlc -I src/ $(RESULT).cma -o test $(SOURCES_DIR)/test.ml 
+clean-test:
+	$(RM) test
+
 
 include $(OCAMLMAKEFILE)
--- src/test.ml
+++ src/test.ml
@@ -1,6 +1,8 @@
+let log module_name =
+  Printf.printf "Testing module %s ...\n" module_name
 
 let test_priority_queue () =
-  print_endline "testing PriorityQueue" ;
+  log "PriorityQueue" ;
   let rec make q = function
   | 0 -> q
   | n -> 
@@ -18,4 +20,28 @@ let test_priority_queue () =
   assert (List.length l = len) ;
   assert (check_sorted l )
 
-let () = test_priority_queue () ;;
+
+let test_utils () = 
+  log "Utils" ;
+  assert (Utils.range 0 4 = [0;1;2;3;4]) ;
+  assert (Utils.range 42 42 = [42] );
+  assert (Utils.range 5 4 = []);
+  assert (Utils.range 1 6 ~step:2 = [1;3;5]) ;
+  assert (Utils.rev_range 5 1 = [5;4;3;2;1]) ;
+  assert (Utils.rev_range 8 1 ~step:3 = [8;5;2]) ;
+  assert (Utils.rev_range 1 2 = []) ;
+
+  let f x = x * x and g x = x + 1 in
+  assert ( (Utils.(-|) f g) 2 = 9)
+
+
+
+let tests = [
+  test_priority_queue ;
+  test_utils ;
+] 
+
+let do_tests () = 
+  List.iter (fun test_fun -> test_fun ()) tests ;;
+
+let () = do_tests () ;;
